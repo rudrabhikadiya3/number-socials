4b577855ba7dea390029200e0fc56ad05a61ae05
Rudra Bhikadiya <irudradev@gmail.com>
2025-04-02 14:47:47 +0530

    feat: enable rich colors for Toaster component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <QueryProvider>{children}</QueryProvider>
-        <Toaster />
+        <Toaster richColors />
       </body>
     </html>
   )
